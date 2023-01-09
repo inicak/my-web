be4372cb790f01a3f002a8ccc9c21fb581a0271d
inicak <ivk2107@barnard.edu>
2023-01-09 12:07:59 -0500

    my website!

---
+++ components/homepage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <center>
+        <h1>What up, world?</h1>
+        <p>Follow me on Instagram!</p>
+        <a href="https://www.instagram.com/inicakotasthane/" target="_blank">
+			right over here :) </a>
+        </center>
+    </div>
+</template>
